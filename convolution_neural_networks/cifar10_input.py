e89a19e90b78c0a540866c581fa5c09a659230a1
huanranxue <huanranxue@creditease.cn>
2017-03-29 19:15:34 +0800

    cifar10 input

---
+++ convolution_neural_networks/cifar10_input.py
@@ -0,0 +1,10 @@
+import os
+import tensorflow as tf
+
+
+def distored_inputs(data_dir, batch_size):
+    filenames = [os.path.join(data_dir, 'data_batch_%d.bin') for x in range(1, 6)]
+    for f in filenames:
+        if not tf.gfile.Exists(f):
+            raise ValueError('Failed to find file ' + f)
+    filename_queue = tf.train.string_input_producer(filenames)
