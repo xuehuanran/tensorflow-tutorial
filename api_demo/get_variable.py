4263c515dcefe96b020eea6759c080b1f9e8c3f4
xuehuanran <935254102@qq.com>
2017-04-08 00:12:39 +0800

    get_variable

--- api_demo/get_variable.py
+++ api_demo/get_variable.py
@@ -1,3 +1,14 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
-filter_weight = tf.get_variable('weights', [5, 5, 3, 16], )
+filter_weight = tf.get_variable('weights', [5, 5, 3, 16], initializer=tf.truncated_normal_initializer(stddev=0.1))
+
+session = tf.Session()
+
+initializer = tf.global_variables_initializer()
+session.run(initializer)
+
+filter_weight_value = session.run(filter_weight)
+
+plt.plot(filter_weight_value.reshape([1200]))
+plt.show()
