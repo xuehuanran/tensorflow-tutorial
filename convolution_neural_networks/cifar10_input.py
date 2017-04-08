1b671afd780b4be89145b986a6fde0b5f8aabec0
xuehuanran <935254102@qq.com>
2017-04-08 20:48:49 +0800

    cifar10_input

--- convolution_neural_networks/cifar10_input.py
+++ convolution_neural_networks/cifar10_input.py
@@ -1,13 +1,38 @@
 import os
 import tensorflow as tf
+from six.moves import xrange
 
 
 def read_cifar10(filename_queue):
-    pass
+
+    class CIFAR10Record(object):
+        pass
+    result = CIFAR10Record()
+
+    label_bytes = 1
+    result.height = 32
+    result.width = 32
+    result.depth = 3
+    image_bytes = result.height * result.width * result.depth
+    record_bytes = label_bytes + image_bytes
+
+    reader = tf.FixedLengthRecordReader(record_bytes=record_bytes)
+    result.key, value = reader.read(filename_queue)
+
+    record_bytes = tf.decode_raw(value, tf.uint8)
+    result.label = tf.cast(tf.strided_slice(record_bytes, [0], [label_bytes]), tf.int32)
+
+    depth_major = tf.reshape(
+        tf.strided_slice(record_bytes, [label_bytes],
+                         [label_bytes + image_bytes]),
+        [result.depth, result.height, result.width])
+    result.uint8image = tf.transpose(depth_major, [1, 2, 0])
+
+    return result
 
 
 def distored_inputs(data_dir, batch_size):
-    filenames = [os.path.join(data_dir, 'data_batch_%d.bin') for x in range(1, 6)]
+    filenames = [os.path.join(data_dir, 'data_batch_%d.bin') for x in xrange(1, 6)]
     for f in filenames:
         if not tf.gfile.Exists(f):
             raise ValueError('Failed to find file ' + f)
