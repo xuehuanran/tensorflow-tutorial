48016598ee129b9e1e685c3d718de18baffa39d9
huanranxue <huanranxue@creditease.cn>
2017-04-14 12:14:51 +0800

    modify exponential decay

--- api_demo/exponential_decay.py
+++ api_demo/exponential_decay.py
@@ -1,10 +1,22 @@
 import tensorflow as tf
 
-global_step = tf.Variable(initial_value=0, trainable=False)
-start_learning_rate = 0.1
-learning_rate = tf.train.exponential_decay(start_learning_rate, global_step, 100000, 0.96, staircase=True)
+global_step = tf.Variable(initial_value=1, trainable=False, dtype=tf.float32)
+start_learning_rate = 1.0
+mu = 0.9
+exp_mu = tf.train.exponential_decay(1.0, global_step, 1.0, mu)
+learning_rate = tf.divide(start_learning_rate, (1 - exp_mu))
+
+x = tf.Variable(tf.constant([0], dtype=tf.float32))
+loss = tf.pow(x, 2.0)
+
+train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
 
 session = tf.Session()
 init = tf.global_variables_initializer()
 session.run(init)
 
+for i in range(10):
+    print('t = ', session.run(global_step))
+    print("exp_mu = ", session.run(exp_mu))
+    print('learning_rate = ', session.run(learning_rate))
+    session.run(train_step)
